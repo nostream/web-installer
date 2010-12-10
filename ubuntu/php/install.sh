9f9efa601e2c470b85a033c48ef05169603eb881
Hidden <zzdhidden@gmail.com>
2010-12-10 13:20:42 +0800

    Start php cgi when installed

--- ubuntu/php/install.sh
+++ ubuntu/php/install.sh
@@ -7,3 +7,4 @@ cp ${ROOT_PATH}/php/php-cgi.conf /etc/default/php-cgi
 cp ${ROOT_PATH}/php/php-cgi /etc/init.d/php-cgi
 chmod +x /etc/init.d/php-cgi
 update-rc.d php-cgi defaults
+service php-cgi start
